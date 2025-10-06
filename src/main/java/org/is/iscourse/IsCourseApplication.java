cca14766c4c26a08bc302288c7814b0697faf2ea
mryrtry <maks01-16@mail.ru>
2025-10-06 10:11:44 +0300

    initial commit

---
+++ src/main/java/org/is/iscourse/IsCourseApplication.java
@@ -0,0 +1,13 @@
+package org.is.iscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/is/iscourse/IsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.is.iscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
