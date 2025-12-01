6aefd3eac6264f770f95778ac02a3011ca20d892
Davletov Timur <145569125+melonegoist@users.noreply.github.com>
2025-12-01 12:53:08 +0300

    Add JWT authentication and login endpoint

--- src/main/java/org/is/iscourse/service/UserService.java
+++ src/main/java/org/is/iscourse/service/UserService.java
@@ -4,15 +4,22 @@ import org.is.iscourse.model.entity.User;
 import org.is.iscourse.model.dto.UserDto;
 import org.is.iscourse.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class UserService {
     
     @Autowired
-    private UserRepository userRepository; 
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     
     
     public User registerUser(UserDto userDto) {
@@ -23,13 +30,22 @@ public class UserService {
         if (userRepository.existsByUsername(userDto.getUsername())) {
             throw new RuntimeException("Username already taken: " + userDto.getUsername());
         }
-        
+
         User user = new User();
         user.setUsername(userDto.getUsername());
         user.setEmail(userDto.getEmail());
-        user.setPasswordHash(userDto.getPassword()); // later change to hash
+        user.setPasswordHash(passwordEncoder.encode(userDto.getPassword()));
         user.setFullName(userDto.getFullName());
-        
+
         return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void updateLastLogin(User user) {
+        user.setLastLogin(LocalDateTime.now());
+        userRepository.save(user);
     }
 }
